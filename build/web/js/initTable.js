b013e01229ec1075ff0a0bddf8ea89ad21ebea71
Francisco Cotto <lancegrs@gmail.com>
2018-11-12 15:54:59 -0600

    subida inicial

---
+++ build/web/js/initTable.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    datatable();
+    datetimepicker();
+});
+
+var datatable = function () {
+
+   $('.table.tabler').not('.loadJson').each(function () {
+        $(this).DataTable({
+            'scrollX': true,
+            'ordering': false,
+            "searching": false,           
+            "bLengthChange": false,
+            'scrollCollapse': true,
+        });
+    });
+};
+
+var datetimepicker = function () {
+    $('.datetimePicker').each(function () {
+        $(this).datetimepicker({
+            'format': 'DD/MMM/YYYY',
+            'ignoreReadonly': true,
+            'allowInputToggle': true
+        });
+    });
+};
---
+++ build/web/js/scripts2.js
@@ -0,0 +1,16 @@
+document.write('<script type="text/javascript" src="../js/jquery.min.js"></script>');
+document.write('<script type="text/javascript" src="../js/bootstrap.js"></script>');
+document.write('<script type="text/javascript" src="../js/jquery.dataTables.js"></script>');
+document.write('<script type="text/javascript" src="../js/dataTables.bootstrap.js"></script>')
+document.write('<script type="text/javascript" src="../js/highlight.js"></script>');
+document.write('<script type="text/javascript" src="../js/main.js"></script>');
+document.write('<script type="text/javascript" src="../js/bootstrap-switch.js"></script>');
+document.write('<script type="text/javascript" src="../js/jquery.validate.js"></script>');
+document.write('<script type="text/javascript" src="../js/formvalidate.js"></script>');
+document.write('<script type="text/javascript" src="../js/waitingfor.js"></script>');
+document.write('<script type="text/javascript" src="../js/bootstrap-dialog.js"></script>');
+document.write('<script type="text/javascript" src="../js/moment.js"></script>');
+document.write('<script type="text/javascript" src="../js/bootstrap-datetimepicker.js"></script>');
+document.write('<script type="text/javascript" src="../js/initTable.js"></script>');
+
+
